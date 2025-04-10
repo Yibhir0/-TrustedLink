c77f9a58a27ebb2c16b33d6e557795ec7c10e600
unknown <z_badawy@live.concordia.ca>
2025-04-10 10:36:05 -0400

    fix for linux: case sensitive import

--- server/routes/UserRoutes.js
+++ server/routes/UserRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { createCustomer, getAllCustomers } from '../controllers/userController.js';
+import { createCustomer, getAllCustomers } from '../controllers/UserController.js';
 
 const router = express.Router();
 
